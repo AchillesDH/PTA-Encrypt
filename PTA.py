1a20cb2b5b4532ca21d3150086c38e868f13afdd
AchillesDH <dyl.hearn@gmail.com>
2018-06-04 10:04:45 +0100

    Pushing PTA.py using File Upload.
    Started work on the decryption, however there are still a few issues to be resolved.

--- PTA.py
+++ PTA.py
@@ -30,68 +30,133 @@ def wrapOrd(i):
 		i = i + 97
 		return i
 
+def encrypt():
+        print("[+] ENCRYPTING")
+        plaintext = raw_input('Input plaintext to encrpyt: ')
+        ciphertext = ''
+        print("")
+        print("Plaintext:  {0}").format(plaintext)
+        
+        plaintext = plaintext.lower()
+        plaintext = list(plaintext)
 
-plaintext = raw_input('Input plaintext to encrpyt: ')
-ciphertext = ''
+        last = int(len(plaintext))
+        
+        for i in range(0, last):
+                
+                plaintext[i] = ord(plaintext[i]) - 97
+                
+                if (i % 9 == 0):
+                        element = plaintext[i] + x1
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                
+                if (i % 9 == 1):
+                        element = plaintext[i] + y1
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
 
-print("")
-print("Plaintext:  {0}").format(plaintext)
+                if (i % 9 == 2):
+                        element = plaintext[i] + z1
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
 
-plaintext = plaintext.lower()
-plaintext = list(plaintext)
+                if (i % 9 == 3):
+                        element = plaintext[i] + x2
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                                
+                if (i % 9 == 4):
+                        element = plaintext[i] + y2
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                                
+                if (i % 9 == 5):
+                        element = plaintext[i] + z2
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
 
-last = int(len(plaintext))
+                if (i % 9 == 6):
+                        element = plaintext[i] + x3
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                                
+                if (i % 9 == 7):
+                        element = plaintext[i] + y3
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                                
+                if (i % 9 == 8):
+                        element = plaintext[i] + z3
+                        element = chr(wrapOrd(element))
+                        ciphertext += element
+                        
+        return ciphertext
 
-for i in range(0, last):
-	if(plaintext[i] == ' '):
-		ciphertext += ' '
-		continue
-		
-	plaintext[i] = ord(plaintext[i]) - 97
-	
-	if (i % 9 == 0):
-		element = plaintext[i] + x1
-		element = chr(wrapOrd(element))
-		ciphertext += element
-	
-	if (i % 9 == 1):
-		element = plaintext[i] + y1
-		element = chr(wrapOrd(element))
-		ciphertext += element
+def decrypt():
+        print("[+] DECRYPTING")
+        ciphertext = raw_input('Input ciphertext to decrpyt: ')
+        plaintext = ''
+        print("")
+        print("Ciphertext:  {0}").format(ciphertext)
 
-	if (i % 9 == 2):
-		element = plaintext[i] + z1
-		element = chr(wrapOrd(element))
-		ciphertext += element
+        ciphertext = ciphertext.lower()
+        ciphertext = list(ciphertext)
 
-	if (i % 9 == 3):
-		element = plaintext[i] + x2
-		element = chr(wrapOrd(element))
-		ciphertext += element
-			
-	if (i % 9 == 4):
-		element = plaintext[i] + y2
-		element = chr(wrapOrd(element))
-		ciphertext += element
-			
-	if (i % 9 == 5):
-		element = plaintext[i] + z2
-		element = chr(wrapOrd(element))
-		ciphertext += element
+        last = int(len(ciphertext))
+        
+        for i in range(0, last):
+                
+                ciphertext[i] = ord(ciphertext[i]) - 97
+                
+                if (i % 9 == 0):
+                        element = ciphertext[i] - x1
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                
+                if (i % 9 == 1):
+                        element = ciphertext[i] - y1
+                        element = chr(wrapOrd(element))
+                        plaintext += element
 
-	if (i % 9 == 6):
-		element = plaintext[i] + x3
-		element = chr(wrapOrd(element))
-		ciphertext += element
-			
-	if (i % 9 == 7):
-		element = plaintext[i] + y3
-		element = chr(wrapOrd(element))
-		ciphertext += element
-			
-	if (i % 9 == 8):
-		element = plaintext[i] + z3
-		element = chr(wrapOrd(element))
-		ciphertext += element
+                if (i % 9 == 2):
+                        element = ciphertext[i] - z1
+                        element = chr(wrapOrd(element))
+                        plaintext += element
 
-print("Ciphertext: {0}").format(ciphertext)
+                if (i % 9 == 3):
+                        element = ciphertext[i] - x2
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                                
+                if (i % 9 == 4):
+                        element = ciphertext[i] - y2
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                                
+                if (i % 9 == 5):
+                        element = ciphertext[i] - z2
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+
+                if (i % 9 == 6):
+                        element = ciphertext[i] - x3
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                                
+                if (i % 9 == 7):
+                        element = ciphertext[i] - y3
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                                
+                if (i % 9 == 8):
+                        element = ciphertext[i] - z3
+                        element = chr(wrapOrd(element))
+                        plaintext += element
+                        
+        return plaintext
+
+encrypted_data = encrypt()
+print("Encrypted data: {}\n").format(encrypted_data)
+decrypted_data = decrypt()
+print("Decrypted data: {}\n").format(decrypted_data)
